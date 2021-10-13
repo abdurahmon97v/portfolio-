b6d9b2f6e38bcc70b529521ae908c5ddcf199027
abdumovi <abdumovi@gmail.com>
2021-10-13 15:31:41 +0500

    home

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('.hero__btn');
+let header = document.querySelector('.wrapper-big__hearder');
+var typed = new Typed('.hero__typer', {
+  strings:['Student','Developer'],
+  typeSpeed:50,
+  backSpeed:50,
+  loop:true
+});
+
+
+btn.addEventListener('click',function(){
+  header.classList.toggle('translet')
+})
+
+
+
+
